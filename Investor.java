6c2635b101fadd85e9a2acd9861d4e15eab756f1
Stefan Chao <Stefan.chao@kcl.ac.uk>
2017-11-29 07:53:21 +0000

    Finished most of the implementation except for calculations

--- Investor.java
+++ Investor.java
@@ -1,34 +1,46 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Investor {
-    public Investor(){
+  Map<String, Bond> purchased;
+  public Investor(){
+    purchased = new HashMap<String, Bond>();
+  }
 
+  public void purchaseBonds(String bond){
+    purchased.put(bond, (Main.getBonds().get(bond)));
+  }
+
+  public void browseBonds(ArrayList<Bond>bondList){
+
+  }
+
+  public String getPurchasedBonds(){
+    String ret = "d";
+    for (Map.Entry<String, Bond> entry : purchased.entrySet()) {
+      String key = entry.getKey();
+      Object value = entry.getValue();
+      ret += key + "\n";
     }
- 
-    public void purchaseBonds(Bond bond){
-
-    }
-
-    public void browseBonds(ArrayList<Bond>bondList){
-
-    }
-
-    build AttayList<Bond> getPurchasedBonds(){
-
-    }
-
-    public double calcPayouts(Bond bond){
-
-    }
-
-    public int calcMacaulay(Bond bond){
-
-    }
-
-    public double calcInternalRateOfReturn(Bond bond){
-
-    }
-
-    public double getBondValue(Bond bond, double inflationRate){
-
-    }
-}	
+    return ret;
+  }
+  public boolean isValid(String bond){ //check if key exists in purchased
+    return (purchased.get(bond) != null); //returns true if it does
+  }
+  // public double calcPayouts(Bond bond){
+  //
+  // }
+  //
+  // public int calcMacaulay(Bond bond){
+  //
+  // }
+  //
+  // public double calcInternalRateOfReturn(Bond bond){
+  //
+  // }
+  //
+  // public double getBondValue(Bond bond, double inflationRate){
+  //
+  // }
+}
--- Main.java
+++ Main.java
@@ -1,23 +1,107 @@
 import java.util.*;
-class Main{
-    
-    Map<String, Bond>listOfBonds;    
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+class Main {
 
-    public static void Main(String[]args){
+  static Map < String, Bond > listOfBonds = new HashMap < String, Bond > ();
 
+  public static void main(String[] args) {
+
+    Investor user = new Investor();
+
+    while (true) {
+
+      System.out.print("OSD_Project : ");
+
+      Scanner scan = new Scanner(System.in);
+      String input = scan.nextLine();
+      //SYSTEM COMMANDS
+      if (input.matches("[>][a-zA-Z]+[\\/]\\d+(.\\d\\d)[\\/]\\d+(.\\d\\d)")) { //does not accept spaces in string atm
+        String[] parts = input.substring(1, input.length()).split("/");
+        addBond(parts[0], Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+      } else if (input.equals(">help")) {
+        help();
+      } else if (input.equals(">exit")) {
+        System.out.println("Are you sure? [Y/n]");
+        Scanner exit = new Scanner(System.in);
+        String input2 = exit.nextLine();
+        if (input2.equalsIgnoreCase("Y"))
+          System.exit(0);
+      } else if (input.equals(">browse")){
+        for (Map.Entry<String, Bond> entry : listOfBonds.entrySet()) {
+          String key = entry.getKey();
+          Object value = entry.getValue();
+          System.out.println(key);
+        }
+      }
+      //INVESTOR COMMANDS
+      else if (input.matches("[>](purchase)[\\/][a-zA-Z]+")){
+        String[] parts = input.substring(1, input.length()).split("/");
+        if(!isValid(parts[1])){ //test for valid key
+          System.out.println("Bond not found");
+        } else {
+          System.out.println("Are you sure you want to purchase " +parts[1]+ "? [Y/n]");
+          Scanner exit = new Scanner(System.in);
+          String input2 = exit.nextLine();
+          if (input2.equalsIgnoreCase("Y")){
+            user.purchaseBonds(parts[1]);
+            System.out.println("Bond purchased");
+          }
+        }
+      } else if (input.equals(">purchased")){
+        System.out.println(user.getPurchasedBonds());
+      } else if (input.equals(">payouts")){
+
+      } else if (input.equals(">macaulay")){
+
+      } else if (input.equals(">IRR")){
+
+      } else {
+        System.out.println("Command not found, please type >help for a list of available commands");
+      }
     }
+  }
 
-    public void addBond(String name, double price, double coupon){
-	//get purchase date
-	m.put(name, new Bond(name, price, coupon);
+
+  public static boolean isValid(String bond){ //check if key exists in purchased
+    return (listOfBonds.get(bond) != null); //returns true if it does
+  }
+
+  public static void addBond(String name, double price, double coupon) {
+        //add purchase date
+    System.out.println("Confirm that you want to create a new bond ");
+    System.out.println("Name: " + name + "\nPrice: " + price + "\nCoupon: " + coupon);
+    System.out.println("Are you sure? [Y/n]");
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine(); //add try/catch to detect for ints
+    if (input.equalsIgnoreCase("Y")) {
+      System.out.println("New bond added");
+      listOfBonds.put(name, new Bond(name, price, coupon, 1, 100));
+    } else { //cancels if any string other than Y or y is inputted
+      System.out.println("Operation cancelled");
+      return;
     }
+  }
 
-    public ArrayList<Bond> getBonds(){
+  public static Map <String, Bond> getBonds() {
+    return listOfBonds;
+  }
+    //
+    // public double getInflation(){
+    //
+    // }
 
-    }
-
-    public double getInflation(){
-
-    }
+  public static void help() {
+    System.out.println("+-----------------------------HELP-----------------------------+");
+    System.out.println("|To add a bond, type '>[Bond name]/[Bond price]/[Bond coupon]' |");
+    System.out.println("|To exit the system, type '>exit'                              |");
+    System.out.println("|To browse current available bonds, type '>browse'             |");
+    System.out.println("|If you wish to buy a bond, type '>purchase/[Bond name]'       |");
+    System.out.println("|If you wish to view purchased bonds, type '>purchased'        |");
+    System.out.println("|type '>payouts'                                               |");
+    System.out.println("|type '>macaulay'                                              |");
+    System.out.println("|type '>IRR'                                                   |");
+    System.out.println("+--------------------------------------------------------------+");
+  }
 }
